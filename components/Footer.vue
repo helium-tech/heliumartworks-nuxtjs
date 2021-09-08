b0f9e81cf80e5fe971be67d74c08837187a00d45
Helium Tech <dev.helium.tech@gmail.com>
2021-09-09 00:13:33 +0200

    add authentication

---
+++ components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- Footer-->
+    <footer class="site-footer bg-gray">
+      <div class="container">
+        <div class="divider"></div>
+        <div class="site-info py-2 navbar">
+          <section class="navbar-section">
+            <div class="site-branding mr-2">
+              <router-link to="/" class="logo"
+                ><span class="text-yellow">helium</span>artworks</router-link
+              >
+            </div>
+          </section>
+          <section>
+            <a href="#" class="btn btn-link">Support et Contact</a>
+            <a href="#" class="btn btn-link">Conditions d'utilisation</a>
+          </section>
+          <section class="navbar-section">
+            <h6>©2021 helium</h6>
+          </section>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
