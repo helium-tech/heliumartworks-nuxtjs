276a1f9350ff2c19ae09d7e0428e498cf96d1aab
Gino <gsotongbe@gmail.com>
2021-12-13 21:59:01 +0000

    Change layout "vendre oeuvre"

--- pages/vendre-mes-oeuvres.vue
+++ pages/vendre-mes-oeuvres.vue
@@ -11,11 +11,37 @@
                     <div class="travel__subtitle">Lorem Ipsum</div>
                     <div class="travel__content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</div>
                   </div>
+                </div><a class="button travel__button" href="#">Ouvrir un compte</a>
+              </div>
+              <div class="travel__col">
+                <div class="travel__wrapper">
+                  <div class="travel__group">
+                    <img class="img-responsive fit-img l-rounded" src=@/assets/images/art-cat.jpg alt="Categorie Art">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="travel__row">
+              <div class="travel__col">
+                <div class="travel__wrapper">
+                  <div class="travel__group">
+                    <img class="img-responsive fit-img l-rounded" src=@/assets/images/art-cat.jpg alt="Categorie Art">
+                  </div>
+                </div>
+              </div>
+              <div class="travel__col">
+                <div class="travel__list">
                   <div class="travel__item">
                     <div class="travel__number" style="background-color: #92A5EF;">02</div>
                     <div class="travel__subtitle">Lorem ipsum dolor sit amet</div>
                     <div class="travel__content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</div>
                   </div>
+                </div><a class="button travel__button" href="#">Ouvrir un compte</a>
+              </div>
+            </div>
+            <div class="travel__row">
+              <div class="travel__col">
+                <div class="travel__list">
                   <div class="travel__item">
                     <div class="travel__number" style="background-color: #58C27D;">03</div>
                     <div class="travel__subtitle">Lorem ipsum dolor sit amet</div>
